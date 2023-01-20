c335774d5f589192bed9dbde07b6caadcd2d22f7
ed mesbahi <mesbahi.ed@outlook.com>
2023-01-20 17:49:34 +0100

    add delete button to each list element

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,3 +6,26 @@ btnlogout.addEventListener('click',() => {
 btnAdd.addEventListener("click",() => {
     fetch("/users/createTask")
 }) */
+const list_button_delete = document.getElementsByClassName("btndelete");
+var arr = Array.prototype.slice.call(list_button_delete);
+arr.forEach(btn => {
+    const taskId = btn.parentElement.id;
+    console.log(taskId);
+    btn.addEventListener('click', () => {
+        fetch('/users/deleteTask', {
+            method: 'POST',
+            body: JSON.stringify({taskId:taskId}),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+            credentials:'include'
+        }).then(function (response) {
+            if (response.ok) {
+                btn.parentElement.remove();
+            }
+        })
+        .catch(function (error) {
+            console.warn('Something went wrong.', error);
+        });
+    })
+});
--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ const Task = require('../models/Task');
 var bcrypt = require('bcrypt');
 var router = express.Router();
 var passport = require("passport");
-var {ensurAuthenticated,forwardAuthenticated } = require('../config/auth');
+var { ensurAuthenticated, forwardAuthenticated } = require('../config/auth');
 /* Login page. */
-router.get('/login',forwardAuthenticated, function (req, res, next) {
+router.get('/login', forwardAuthenticated, function (req, res, next) {
   res.render('login');
 });
 // register page
-router.get('/register',forwardAuthenticated, function (req, res, next) {
+router.get('/register', forwardAuthenticated, function (req, res, next) {
   res.render('register');
 });
 router.post('/register', (req, res) => {
@@ -74,53 +74,58 @@ router.post('/login', (req, res, next) => {
 router.get('/logout', function (req, res, next) {
 
   req.logout(function (err) {
-    if (err) { return next(err); }
+    if (err) { 
+      return next(err); 
+    }
   })
-  req.flash("success_msg","you logged out with success");
+  req.flash("success_msg", "you logged out with success");
   res.redirect('/users/login');
 });
 // create task handler
-router.post('/createTask',ensurAuthenticated,(req,res) => {
-  const {task} = req.body;
-  if(task != ''){
+router.post('/createTask', ensurAuthenticated, (req, res) => {
+  const { task } = req.body;
+  if (task != '') {
     var newTask = new Task({
-      owner:req.session.passport.user,
-      content:task
+      owner: req.session.passport.user,
+      content: task
     });
     console.log(newTask);
     newTask.save()
-    .then(result => {
-      req.flash("success_msg","added with success")
-      res.redirect("/dashboard");
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(501).send();
-    })
-  
+      .then(result => {
+        req.flash("success_msg", "added with success")
+        res.redirect("/dashboard");
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(501).send();
+      })
+
   } else {
-    res.status(401).send();
+    req.flash("error_msg","please fill out the task field");
+    res.redirect('/dashboard');
   }
-  
+
 })
-router.get("/allTasks",ensurAuthenticated,async (req,res) => {
-  const allTasks = await Task.find({owner:req.session.passport.user},{_id: 1, content: 1, date: 1}).exec();
-  res.render('dashboard', {allTasks});
-  console.log(res.locals)
+router.get("/allTasks", ensurAuthenticated, async (req, res) => {
+  const allTasks = await Task.find({ owner: req.session.passport.user }, { _id: 1, content: 1, date: 1 }).exec();
+  res.render('dashboard', { allTasks });
 });
-router.post("/deleteTask",ensurAuthenticated,async (req,res) => {
-  const {taskId} = req.body;
+router.post("/deleteTask", ensurAuthenticated, async (req, res) => {
+  console.log(req);
+  const { taskId } = req.body;
+  if(taskId)
   if (taskId.match(/^[0-9a-fA-F]{24}$/)) {
     // Yes, it's a valid ObjectId, proceed with `findByIdAndDelete` call.
     Task.findByIdAndDelete(taskId).exec()
-    .then(result => {
-      res.send();
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(502).send();
-    })
-
+      .then(result => {
+        res.send();
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(502).send();
+      })
+  } else {
+    res.status(401).send();
   }
 })
 module.exports = router;
